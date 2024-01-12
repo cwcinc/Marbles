f534b98405368555d6a40bf6101ef85be99556af
Coding With Cameron <137846994+cwcinc@users.noreply.github.com>
2024-01-11 18:19:07 -0800

    Update content.js

--- content.js
+++ content.js
@@ -2,3 +2,12 @@
 console.log('Content script loaded');
 
 document.body.style.backgroundColor = 'lightblue';
+
+oldscript = document.querySelectorAll("script")[2]
+const newScript = document.createElement('script');
+newScript.text = `
+
+console.log("hi");
+  
+`;
+oldscript.parentNode.replaceChild(newScript, oldscript);
